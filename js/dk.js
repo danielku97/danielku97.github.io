887c5565c258f0510bb688cd47864c9ef9af5869
Daniel Ku <danielku97@gmail.com>
2016-12-13 17:02:38 -0500

    Back up files from Dec 13

--- js/dk.js
+++ js/dk.js
@@ -1,3 +1,21 @@
 $(".btn").mouseup(function(){
     $(this).blur();
 })
+
+$(window).on('resize', function() {
+    if($(window).width() > 980) {
+        console.log('small')
+        $('#flipp-eq-height').addClass('row-eq-height');
+    }else{
+        console.log('big')
+        $('#flipp-eq-height').removeClass('row-eq-height');
+    }
+})
+
+$(window).on('resize', function() {
+    if($(window).width() > 980) {
+        $('#dh-eq-height').addClass('row-eq-height');
+    }else{
+        $('#dh-eq-height').removeClass('row-eq-height');
+    }
+})
